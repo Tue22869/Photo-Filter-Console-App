6fd0319d23a46d1f51ae47843e30e011e1ec2d3b
tue22869 <mctuez@gmail.com>
2022-03-31 20:00:29 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/photo_project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/photo_project.dir/filter.cpp.o"
+  "CMakeFiles/photo_project.dir/image.cpp.o"
+  "CMakeFiles/photo_project.dir/main.cpp.o"
+  "photo_project"
+  "photo_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/photo_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
